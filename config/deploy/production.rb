16ebeea1c655b4d86ec783f2b4a0238431a38fab
柴田 大介 <oujirou6kg@gmail.com>
2016-12-12 18:36:17 +0900

    capistrano編集
    capistrano編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,9 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '00.00.000.000', user: 'naoki', roles: %w{app db web} 
+server '52.199.46.3
+', user: 'daisuke', roles: %w{app db web} 
 
 #デプロイするサーバにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/first_aws_rsa' 
+set :ssh_options, keys: '~/.ssh/key-pair_key_rsa' 
 
 # server-based syntax
 # ======================
